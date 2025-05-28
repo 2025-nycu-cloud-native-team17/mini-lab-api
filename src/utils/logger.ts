53383c8add89b2f9dd8a5a08ee3cbe410ab1555f
mimi-lab <mini-lab@example.com>
2025-05-28 16:52:47 +0800

    /fixbug?: sync write file

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,6 +2,13 @@ import pino from 'pino';
 
 const isCI = process.env.CI === 'true';
 const isDev = process.env.NODE_ENV === 'development';
+const logPath = process.env.LOG_PATH || '/var/log/mini-lab/mini-lab-api.log';
+
+const dest = pino.destination({
+  dest: logPath,
+  sync: true,
+  mkdir: true
+});
 
 const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
